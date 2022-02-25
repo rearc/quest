50dddbba67c6edd0f1dcdebdf9e5136288d23a6f
Jonathan Finley <accounts@jonathanfinley.net>
2022-02-25 17:16:26 -0500

    create new ECR repo

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_ecr_repository" "rearc-quest" {
+  name = "rearc-quest" # Naming my repository
+}
