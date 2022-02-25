88820d17d90dc2e160e18491cfddf2b882f75980
Jonathan Finley <accounts@jonathanfinley.net>
2022-02-25 17:16:56 -0500

    terraform policy and iam

--- main.tf
+++ main.tf
@@ -15,5 +15,57 @@ provider "aws" {
 }
 
 resource "aws_ecr_repository" "rearc-quest" {
-  name = "rearc-quest" # Naming my repository
+  name = "rearc-quest" # Name of Repo
 }
+
+resource "aws_ecs_cluster" "rearc-quest" {
+  name = "rearc-quest" # Name of Cluster
+}
+
+resource "aws_ecs_task_definition" "rearc-quest" {
+  family = "rearc-quest" # Nam of task
+  container_definitions = <<DEFINITION
+  [
+    {
+      "name": "rearc-quest",
+      "image": "${aws_ecr_repository.rearc-quest.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 3000,
+          "hostPort": 3000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
+  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
+  memory                   = 512         # Specifying the memory our container requires
+  cpu                      = 256         # Specifying the CPU our container requires
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
