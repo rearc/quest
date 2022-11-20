ddeab3844cb8d4f3bda5ae4138d5c0aca4003a88
Bennie Mosher <benniemosher@gmail.com>
2022-11-20 12:08:11 -0700

    BAM: Broke out network config

--- terraform/modules/aws-ecs/iam.tf
+++ terraform/modules/aws-ecs/iam.tf
@@ -3,3 +3,7 @@ resource "aws_iam_role" "ecs-task-execution" {
   assume_role_policy = data.aws_iam_policy_document.ecs-task-execution.json
 }
 
+resource "aws_iam_role_policy_attachment" "ecs-task-execution" {
+  role       = aws_iam_role.ecs-task-execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
