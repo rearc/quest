0667271b97a6b1b97667adf6a6eb1a010382a153
trevor costanzi <trevor.costanzi@gmail.com>
2026-03-09 17:12:34 -0700

    fix: terraform fmt after some minor edits

--- tf/main.tf
+++ tf/main.tf
@@ -37,7 +37,7 @@ resource "aws_ecs_task_definition" "quest" {
         }
       ]
 
-}
+    }
   ])
 }
 
--- tf/outputs.tf
+++ tf/outputs.tf
@@ -16,10 +16,10 @@ output "app_url_https" {
 output "check_urls" {
   description = "Quest stage check endpoints"
   value = {
-    index       = "https://${aws_lb.quest.dns_name}/"
-    docker      = "https://${aws_lb.quest.dns_name}/docker"
-    secret_word = "https://${aws_lb.quest.dns_name}/secret_word"
+    index        = "https://${aws_lb.quest.dns_name}/"
+    docker       = "https://${aws_lb.quest.dns_name}/docker"
+    secret_word  = "https://${aws_lb.quest.dns_name}/secret_word"
     loadbalanced = "https://${aws_lb.quest.dns_name}/loadbalanced"
-    tls         = "https://${aws_lb.quest.dns_name}/tls"
+    tls          = "https://${aws_lb.quest.dns_name}/tls"
   }
 }
